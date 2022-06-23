47aa7f080d810accb0ffad39af8c0a95f5c26134
baranowb <baranowb@gmail.com>
2022-06-23 13:28:13 +0200

    EAP8_TMP

--- jobs/vbe_jobs.groovy
+++ jobs/vbe_jobs.groovy
@@ -1,5 +1,24 @@
 //new vbe.Builder(branch: '7.4.x', javaHome: "/opt/oracle/openjdk-11.0.14.1_1").buildAndTest(this)
-//Disable 8, since its set up from custom branch for now
-//new vbe.Builder(branch: '8.0.0.dev', javaHome: "/opt/oracle/openjdk-11.0.14.1_1", gitRepositoryUrl: 'git@github.com:jbossas/jboss-eap8.git',vbeRepositoryNames: '', vbeChannels: '').buildAndTest(this)
-
+new vbe.Builder(jobName: "vbe-eap-tmp-8.0.0.dev", branch: '8.0.0.dev', javaHome: "/opt/oracle/openjdk-11.0.14.1_1", gitRepositoryUrl: 'git@github.com:jbossas/jboss-eap8.git',vbeRepositoryNames: 'jboss-eap-8.0-product-repository', vbeChannels: "${VPE_EAP_8_CHANNEL}").buildAndTest(this)
+//WFCORE for eap8... needs hacks...
+new eap7.Builder(branch:'18.0.x',
+                 jobName: 'vbe-wildfly-core-eap',
+                 gitRepositoryUrl: "git@github.com:jbossas/wildfly-core-eap.git",
+                 javaHome: "/opt/oracle/openjdk-11.0.14.1_1",
+                  customParams: {
+                    stringParam {
+                        name("TEST_OPTS")
+                        defaultValue("-DskipTests -Dversion.org.wildfly.openssl.natives=2.2.0.Final-redhat-00002")
+                    }
+                    stringParam {
+                        name("BUILD_OPTS")
+                        defaultValue("-DskipTests -Dversion.org.wildfly.openssl.natives=2.2.0.Final-redhat-00002")
+                    }
+                    stringParam {
+                        name("MAVEN_OPTS")
+                        defaultValue("-Dmaven.wagon.http.ssl.insecure=true -Dhttps.protocols=TLSv1.2 -DskipTests -Dversion.org.wildfly.openssl.natives=2.2.0.Final-redhat-00002")
+                    }
+                  }
+                ).buildAndTest(this)
+EapView.jobList(this, 'vbe-wildfly', 'vbe-wildfly-core.*')
 EapView.jobList(this, 'vbe', 'vbe-eap.*')
